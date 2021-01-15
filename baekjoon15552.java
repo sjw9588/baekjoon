7e0600ab15cb064e54238a908744d820557b3688
서지웅 <sjw9588@gmail.com>
2021-01-15 23:26:46 +0900

    baekjoon day3

--- baekjoon15552.java
+++ baekjoon15552.java
@@ -1,5 +1,27 @@
 package basic_tutorial;
 
-public class baekjoon15552 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
 
+
+public class baekjoon15552 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(bf.readLine());
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=0;i<t;i++) {
+			StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+			bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+"\n");
+		}
+		bf.close();
+		bw.flush();
+		bw.close();
+	}
 }
+
