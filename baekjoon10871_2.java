971d44bab38513b2cdd34bdf57dde3cf5db82b3c
서지웅 <sjw9588@gmail.com>
2021-01-17 21:43:38 +0900

    baekjoon day4

--- baekjoon10871_2.java
+++ baekjoon10871_2.java
@@ -7,28 +7,24 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class baekjoon10871 {
+public class baekjoon10871_2 {
 	public static void main(String args[])throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int a [] = new int[n];
 		int x = Integer.parseInt(st.nextToken());
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
-			a[i]= Integer.parseInt(st.nextToken());
-		}
-		
-		for(int i=0;i<n;i++) {
-			if(a[i]<x) {
-				bw.write(a[i]+" ");
-			}
+			int k = Integer.parseInt(st.nextToken());
+			if(x > k)
+				bw.write(k+" ");
 		}
 		br.close();
 		bw.flush();
 		br.close();
 	}
+
 }
