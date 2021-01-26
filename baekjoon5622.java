d1ef228626a07e71fd56b01ed09f8859067dcb92
서지웅 <sjw9588@gmail.com>
2021-01-26 19:53:51 +0900

    baekjoon day13

--- baekjoon5622.java
+++ baekjoon5622.java
@@ -1,33 +1,43 @@
 package basic_tutorial;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class baekjoon5622 {
 	public static void main(String[] args) throws IOException {
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String S = br.readLine();
 		int count = 0;
-		int value;
-
-		while ((value = System.in.read()) != '\n') {
-			
-
-			if (value>64 && value <68)
-				count += 3;
-			else if (value > 67 && value <71)
-				count += 4;
-			else if (value > 70 && value <74)
-				count += 5;
-			else if (value > 73 && value <77)
-				count += 6;
-			else if (value > 76 && value <80)
-				count += 7;
-			else if (value > 80 && value <85)
-				count += 8;
-			else if (value > 84 && value <88)
-				count += 9;
-			else if(value >87 && value <92)
-				count += 10;
-
+		for(int i=0; i<S.length(); i++) {
+			switch(S.charAt(i)) {
+			case 'A' : case 'B' : case 'C' :
+				count +=3;
+				break;
+			case 'D' : case 'E' : case 'F' :
+				count +=4;
+				break;
+			case 'G' : case 'H' : case 'I' :
+				count +=5;
+				break;
+			case 'J' : case 'K' : case 'L' :
+				count +=6;
+				break;
+			case 'M' : case 'N' : case 'O' :
+				count +=7;
+				break;
+			case 'P' : case 'Q' : case 'R' : case 'S' :
+				count +=8;
+				break;
+			case 'T' : case 'U' : case 'V' :
+				count +=9;
+				break;
+			case 'W' : case 'X' : case 'Y' : case 'Z' :
+				count +=10;
+				break;
+				
+			};
 		}
 		System.out.print(count);
 	}
