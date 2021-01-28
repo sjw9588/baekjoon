d159fda17b61923c958a330f216132f7662008e0
서지웅 <sjw9588@gmail.com>
2021-01-28 14:12:26 +0900

    baekjoon day15

--- baekjoon1316.java
+++ baekjoon1316.java
@@ -11,23 +11,32 @@ public class baekjoon1316 {
 
 		int n = Integer.parseInt(br.readLine());
 		int arr[] = new int[26];
-	//	char word[] = new char[100];
-		for(int i=0; i<26; i++) {
-			arr[i] = 0;		//중복될 경우 i++해서 배열을 한칸 넘어가는 방법. + 이미 0이 아닌 경우 그룹단어가 아닌 것으로 간주.
+		int cnt = 0;
+		int num = n;
+		for (int i = 0; i < 26; i++) {
+			arr[i] = 0; // 중복될 경우 i++해서 배열을 한칸 넘어가는 방법. + 이미 0이 아닌 경우 그룹단어가 아닌 것으로 간주.
 		}
-
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String k = st.toString();
-			char word[] = k.toCharArray();
-			System.out.println((char)(word[0]-2));
-			System.out.println((char)(word[1]-2));
+			String st = br.readLine();
+			for (int j = 0; j < st.length(); j++) {
+				char k = st.charAt(j);
+
+				if (j + 1 < st.length()) {
+					if (k == st.charAt(j + 1)) {
+						j++;
+					}
+				}
+				else if (arr[k - 'a'] == 0){
+					arr[k - 'a']++;
+				}
+				else if (arr[k - 'a'] != 0) {
+						cnt = --num;
+						System.out.println("cnt : " +cnt);
+						System.out.println("num : " +num);
+				}
+			}
+			System.out.println(cnt);
+			System.out.println(num);
 		}
 	}
 }
-
-
-
-
-
-
