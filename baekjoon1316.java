ddc812d8cedfd85bc55b4efbc58311a0e451e8f7
서지웅 <sjw9588@gmail.com>
2021-01-28 22:56:14 +0900

    baekjoon day15

--- baekjoon1316.java
+++ baekjoon1316.java
@@ -6,37 +6,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class baekjoon1316 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
-		int arr[] = new int[26];
 		int cnt = 0;
-		int num = n;
-		for (int i = 0; i < 26; i++) {
-			arr[i] = 0; // 중복될 경우 i++해서 배열을 한칸 넘어가는 방법. + 이미 0이 아닌 경우 그룹단어가 아닌 것으로 간주.
-		}
-		for (int i = 0; i < n; i++) {
-			String st = br.readLine();
-			for (int j = 0; j < st.length(); j++) {
-				char k = st.charAt(j);
 
-				if (j + 1 < st.length()) {
-					if (k == st.charAt(j + 1)) {
-						j++;
-					}
-				}
-				else if (arr[k - 'a'] == 0){
-					arr[k - 'a']++;
-				}
-				else if (arr[k - 'a'] != 0) {
-						cnt = --num;
-						System.out.println("cnt : " +cnt);
-						System.out.println("num : " +num);
+		for (int i = 0; i < n; i++) {
+			if (check()) { // check()함수가 true이면 그룹단어 개수를 하나 증가시킨다.
+				cnt++;
+			}
+		}
+		System.out.print(cnt);
+	}
+
+	public static boolean check() throws IOException {
+		boolean check[] = new boolean[26]; // 초기값은 false('중복안됨'표시). true '중복됨' 표시.
+		int num = 0;
+
+		String str = br.readLine();
+		int len = str.length();
+
+		for (int i = 0; i < len; i++) {
+			char k = str.charAt(i);
+
+			if (k != num) {
+				if (check[k - 'a'] == false) {
+					check[k - 'a'] = true;
+					num = k;
+				} 
+				else if (check[k - 'a'] == true) {
+					return false;
 				}
 			}
-			System.out.println(cnt);
-			System.out.println(num);
 		}
+		return true;
 	}
 }
