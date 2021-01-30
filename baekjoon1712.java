485846cca195ca0a4e229db1af92cac976ab987e
서지웅 <sjw9588@gmail.com>
2021-01-30 23:21:10 +0900

    baekjoon day16

---
+++ baekjoon1712.java
@@ -0,0 +1,24 @@
+package basic_tutorial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class baekjoon1712 {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int fix = Integer.parseInt(st.nextToken());
+		int one = Integer.parseInt(st.nextToken());
+		int price = Integer.parseInt(st.nextToken());
+		int plus = 0;
+
+		if (one >= price) {
+			plus = -1;
+		}
+		else plus = fix/(price-one)+1;
+		System.out.print(plus);
+	}
+}
