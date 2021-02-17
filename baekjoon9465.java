420cdfa55cf867a43cb5ed5b193dda13d694f5b7
서지웅 <sjw9588@gmail.com>
2021-02-17 23:04:47 +0900

    Dynamic Programming

--- baekjoon9465.java
+++ baekjoon9465.java
@@ -12,22 +12,26 @@ public class baekjoon9465 {
 		int T = Integer.parseInt(br.readLine());
 		for (int i = 0; i < T; i++) {
 			int n = Integer.parseInt(br.readLine());
-			int arr[][] = new int[2][n];
-
+			int arr[][] = new int[n][2];							// 입력할 숫자들로 구성될 배열 생성.
+			int d[][] = new int[n][3];
+			
 			for (int k = 0; k < 2; k++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-				for (int j = 0; j < n; j++) {
-					arr[k][j] = Integer.parseInt(st.nextToken());
+				for (int j = 0; j <n; j++) {
+					arr[j][k] = Integer.parseInt(st.nextToken());
 				}
+			}					//각 열별로 n번째 경우에 oo, ox, xo 각각 세가지 경우의 수에 따른 n-1번째의 경우의 수들의 최댓값을 구한다.
+			d[0][0] = 0;
+			d[0][1] = arr[0][0];
+			d[0][2] = arr[0][1];
+			
+			for(int k=1; k<n; k++) {
+				d[k][0] = Math.max(d[k-1][0], Math.max(d[k-1][1], d[k-1][2]));
+				d[k][1] = Math.max(d[k-1][0], d[k-1][2]) + arr[k][0];
+				d[k][2] = Math.max(d[k-1][0], d[k-1][1]) + arr[k][1];
 			}
-			int temp = 0;
-			for (int j = 0; j < n - 1; j++) {
-				if ((arr[0][j] > arr[0][j + 1]) || (arr[0][j] > arr[1][j])) {
-					arr[0][j + 1] = 0;
-					arr[1][j] = 0;
-				}
-				else if((arr[0][j] > arr[0][j + 1]) || (arr[0][j] > arr[1][j]))
-			}
+			int ans = Math.max(d[n-1][0], Math.max(d[n-1][1], d[n-1][2]));
+			System.out.println(ans);
 		}
 	}
 }
